2c135fc20b38a208f3470fb206af063a762b35b7
Patrick McFadden <pmcfadde@thoughtworks.com>
2022-09-12 15:13:44 -0600

    [pmcfadden] Moving towards a styra client to make future actions easier

--- configure_system.py
+++ configure_system.py
@@ -1,34 +1,5 @@
 import sys
-import os
-import json
-from typing import Dict
-
-import requests
-
-TOKEN_NOT_SET = "TOKEN_NOT_SET"
-STYRA_TOKEN = os.environ.get("STYRA_API_TOKEN", TOKEN_NOT_SET)
-GITHUB_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", TOKEN_NOT_SET)
-STYRA_BASE_URL = "https://thoughtworks.styra.com/v1/"
-COMMON_HEADERS = {
-        "Authorization": f"Bearer {STYRA_TOKEN}",
-        "Content-Type": "application/json"
-        }
-
-def build_request(name: str, environment: str, branch: str):
-    return json.dumps({
-      "type": "template.istio:1.0",
-      "name": name,
-      "description": f"authorization system for the Teams API for Environment: {environment}",
-      "read_only": True,
-      "source_control": {
-        "origin": {
-          "url": "https://github.com/ThoughtWorks-DPS/teams-authorization-policies",
-          "reference": f"refs/heads/{branch}",
-          "credentials": "git/teams-policy-repo-access"
-        }
-      }
-    })
-
+from lib import StyraClient
 
 if len(sys.argv) != 3:
     print("Usage: python configure_system.py <environment> <branch to track>")
@@ -38,57 +9,8 @@ ENVIRONMENT = sys.argv[1]
 BRANCH = sys.argv[2]
 SYSTEM_NAME = f"lab_teams_{ENVIRONMENT}"
 
-if STYRA_TOKEN == TOKEN_NOT_SET:
-    print("Environment variable STYRA_API_TOKEN must be set")
-    sys.exit(1)
+client = StyraClient(ENVIRONMENT)
 
-if GITHUB_TOKEN == TOKEN_NOT_SET:
-    print("Environment variable GITHUB_ACCESS_TOKEN must be set")
-    sys.exit(1)
+client.ensure_git_secret_exists()
 
-
-def create_new_system():
-    headers = COMMON_HEADERS
-    headers['IF-None-Match'] = "*"
-    r = requests.put(STYRA_BASE_URL + f"systems/{SYSTEM_NAME}",
-        timeout=5,
-        data=build_request(SYSTEM_NAME, ENVIRONMENT, BRANCH),
-        headers=headers)
-    if r.status_code != 200:
-        print("Error creating a new system")
-        r.raise_for_status()
-    print("Successfully created new system")
-
-
-def update_existing_system():
-    headers = COMMON_HEADERS
-    headers['Host'] = "thoughtworks.styra.com"
-    r: requests.Response = requests.put(STYRA_BASE_URL + f"systems/{SYSTEM_NAME}",
-        timeout=5,
-        data=build_request(SYSTEM_NAME, ENVIRONMENT, BRANCH),
-        headers=headers)
-    if r.status_code != 200:
-        print("Error updating system")
-        r.raise_for_status()
-    print("Successfully updated system")
-
-secret_payload = {"name": "twdpsio", "secret": GITHUB_TOKEN}
-secret_request = requests.put(STYRA_BASE_URL + 'secrets/git/teams-policy-repo-access',
-        timeout=5,
-        data=secret_payload,
-        headers=COMMON_HEADERS)
-
-if secret_request.status_code != 200:
-    print(f"Error creating secret with status code {secret_request.status_code}")
-    secret_request.raise_for_status()
-else:
-    print("Successfully created secret for git credentials")
-
-current_system_request = requests.get(STYRA_BASE_URL + f"systems/{SYSTEM_NAME}",
-        timeout=5,
-        headers=COMMON_HEADERS)
-
-if current_system_request.status_code == 200:
-    update_existing_system()
-else:
-    create_new_system()
+client.create_or_update_system(SYSTEM_NAME, BRANCH)
---
+++ lib/__init__.py
@@ -0,0 +1,92 @@
+import os
+import json
+from typing import Dict, Union
+
+import requests
+
+TOKEN_NOT_SET = "TOKEN_NOT_SET"
+STYRA_TOKEN = os.environ.get("STYRA_API_TOKEN", TOKEN_NOT_SET)
+GITHUB_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", TOKEN_NOT_SET)
+COMMON_HEADERS = {
+        "Authorization": f"Bearer {STYRA_TOKEN}",
+        "Content-Type": "application/json"
+        }
+
+class StyraClient:
+    def __init__(self, environment: str, host: str='thoughtworks.styra.com') -> None:
+        self.prefix = f"https://{host}/v1/"
+        self.env = environment
+        self.headers = COMMON_HEADERS.copy()
+        self.headers['Host'] = host
+        if STYRA_TOKEN == TOKEN_NOT_SET:
+            raise Exception("STYRA_API_TOKEN env variable not set")
+        if GITHUB_TOKEN == TOKEN_NOT_SET:
+            raise Exception("GITHUB_ACCESS_TOKEN env variable is not set")
+
+    def create_or_update_system(self, name: str, branch: str):
+        if self.get_system(name) is None:
+            self._create_system(name, branch)
+        else:
+            self._update_system(name, branch)
+
+    def get_system(self, name: str) -> Union[Dict, None]:
+        request = requests.get(self.prefix + f"systems/{name}",
+                timeout=5, headers=self.headers)
+        if request.status_code == 404:
+            return None
+        return request.json()
+
+    def ensure_git_secret_exists(self):
+        secret_payload = {"name": "twdpsio", "secret": GITHUB_TOKEN}
+        secret_request = requests.put(self.prefix + 'secrets/git/teams-policy-repo-access',
+                timeout=5,
+                data=secret_payload,
+                headers=self.headers)
+
+        if secret_request.status_code != 200:
+            print(f"Error creating secret with status code {secret_request.status_code}")
+            secret_request.raise_for_status()
+        else:
+            print("Successfully created secret for git credentials")
+
+
+    def _create_system(self, name: str,  branch: str):
+        headers = self.headers.copy()
+        headers['IF-None-Match'] = "*"
+        request = requests.put(self.prefix + f"systems/{name}",
+            timeout=5,
+            data=self._build_system_request(name, self.env, branch),
+            headers=headers)
+        if request.status_code != 200:
+            print("Error creating a new system")
+            request.raise_for_status()
+        print("Successfully created new system")
+
+
+    def _update_system(self, name: str, branch: str):
+        request = requests.put(self.prefix + f"systems/{name}",
+            timeout=5,
+            data=self._build_system_request(name, self.env, branch),
+            headers=self.headers)
+        if request.status_code != 200:
+            print("Error updating system")
+            request.raise_for_status()
+        print("Successfully updated system")
+
+
+    def _build_system_request(self, name: str, environment: str, branch: str):
+        return json.dumps({
+          "type": "template.istio:1.0",
+          "name": name,
+          "description": f"authorization system for the Teams API for Environment: {environment}",
+          "read_only": True,
+          "source_control": {
+            "origin": {
+              "url": "https://github.com/ThoughtWorks-DPS/teams-authorization-policies",
+              "reference": f"refs/heads/{branch}",
+              "credentials": "git/teams-policy-repo-access"
+            }
+          }
+        })
+
+
